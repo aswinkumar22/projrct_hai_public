b6e68b31e407fefaf437715caa294af11d224330
aswinkumar22 <aswinkumar428@gmail.com>
2022-04-13 00:13:41 +0530

    new change

---
+++ terraform/ebs.tf
@@ -0,0 +1,21 @@
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-00ee4df451840fa9d"
+  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+  key_name = "ashwin1234"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 20
+}
